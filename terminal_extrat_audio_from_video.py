f114b1aa581b8a75b6c756a09be86dc34080cf65
Vitor Krewer <47862173+vitorkrewer@users.noreply.github.com>
2023-07-14 17:19:48 -0300

    Update terminal_extrat_audio_from_video.py

--- terminal_extrat_audio_from_video.py
+++ terminal_extrat_audio_from_video.py
@@ -6,8 +6,8 @@ def extract_audio(input_file,output_file):
     audio.write_audiofile(output_file)
 
 # Configuração dos Dados de Entrada e Saída - Especificar caminhos absolutos incluíndo nome do arquivo com extensão
-input_file = 'C:/Users/hp-14-vmk/OneDrive - Grupo Focus/faculdade-focus/equipe-multidisciplinar/reunioes-videos/reuniao-multi-03-07-23.mp4'
-output_file = 'C:/Users/hp-14-vmk/OneDrive - Grupo Focus/faculdade-focus/equipe-multidisciplinar/reunioes-videos/reuniao-multi-03-07-23.mp3'  # Arquivo de áudio de saídas
+input_file = 'video-input.mp4'
+output_file = 'audio-output.mp3'  # Arquivo de áudio de saídas
 
 extract_audio(input_file,output_file)
 print("Áudio extraído com sucesso!")
